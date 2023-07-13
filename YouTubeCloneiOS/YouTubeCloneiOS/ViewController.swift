073f8d68cae9a8b6cf219e09b4aae9e076ee9e87
Jarl Kallagher <z_amr145@hotmail.com>
2023-07-13 04:58:37 -0600

    Creacion del proyecto
    con git

---
+++ YouTubeCloneiOS/YouTubeCloneiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeCloneiOS
+//
+//  Created by Alberto Martinez Ruiz on 13/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
